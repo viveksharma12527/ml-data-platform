7cd25f459d178567a450ad2b26c10f5d86e1e6d7
amjedalsadig <amjedalsadig@hotmail.com>
2025-11-10 02:08:54 +0100

    - modified the db.js to load from env instead of hard coded - fixed the annotation get next image route to POST instead of GET since it uses req.body - added nodemon for better developing

--- backend/AnnotationTool/annotation.routes.js
+++ backend/AnnotationTool/annotation.routes.js
@@ -6,6 +6,6 @@ const authenticatToken = require('../Middlewares/auth.js');
 router.use(authenticatToken);
 
 router.post('/', annotationController.saveAnnotation);
-router.get('/next', annotationController.getNextImage);
+router.post('/next', annotationController.getNextImage);
 
 module.exports = router;
